b1925289df53f6abf555f3fb523ce9ba61c3b61d
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-02-24 19:15:04 +0100

    add HermitOS support
    HermitOS is a library operating system. It it is partly compatible
    to Unix. Consequently, the system interface to unix is revised
    to support HermitOS.

--- tokio/src/net/tcp/socket.rs
+++ tokio/src/net/tcp/socket.rs
@@ -4,6 +4,8 @@ use std::fmt;
 use std::io;
 use std::net::SocketAddr;
 
+#[cfg(target_os = "hermit")]
+use std::os::hermit::io::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, RawFd};
 #[cfg(unix)]
 use std::os::unix::io::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, RawFd};
 use std::time::Duration;
@@ -623,6 +625,7 @@ impl TcpSocket {
     #[cfg(not(any(
         target_os = "fuchsia",
         target_os = "redox",
+        taregt_os = "hermit",
         target_os = "solaris",
         target_os = "illumos",
         target_os = "haiku"
@@ -632,6 +635,7 @@ impl TcpSocket {
         doc(cfg(not(any(
             target_os = "fuchsia",
             target_os = "redox",
+            taregt_os = "hermit",
             target_os = "solaris",
             target_os = "illumos",
             target_os = "haiku"
@@ -684,6 +688,7 @@ impl TcpSocket {
         target_os = "fuchsia",
         target_os = "redox",
         target_os = "solaris",
+        target_os = "hermit",
         target_os = "illumos",
         target_os = "haiku"
     )))]
@@ -692,6 +697,7 @@ impl TcpSocket {
         doc(cfg(not(any(
             target_os = "fuchsia",
             target_os = "redox",
+            target_os = "hermit",
             target_os = "solaris",
             target_os = "illumos",
             target_os = "haiku"
@@ -826,7 +832,7 @@ impl TcpSocket {
     /// ```
     pub async fn connect(self, addr: SocketAddr) -> io::Result<TcpStream> {
         if let Err(err) = self.inner.connect(&addr.into()) {
-            #[cfg(unix)]
+            #[cfg(any(unix, target_os = "hermit"))]
             if err.raw_os_error() != Some(libc::EINPROGRESS) {
                 return Err(err);
             }
@@ -835,9 +841,12 @@ impl TcpSocket {
                 return Err(err);
             }
         }
-        #[cfg(unix)]
+        #[cfg(any(unix, target_os = "hermit"))]
         let mio = {
+            #[cfg(unix)]
             use std::os::unix::io::{FromRawFd, IntoRawFd};
+            #[cfg(target_os = "hermit")]
+            use std::os::hermit::io::{FromRawFd, IntoRawFd};
 
             let raw_fd = self.inner.into_raw_fd();
             unsafe { mio::net::TcpStream::from_raw_fd(raw_fd) }
@@ -891,9 +900,12 @@ impl TcpSocket {
     /// ```
     pub fn listen(self, backlog: u32) -> io::Result<TcpListener> {
         self.inner.listen(backlog as i32)?;
-        #[cfg(unix)]
+        #[cfg(any(unix, target_os = "hermit"))]
         let mio = {
+            #[cfg(unix)]
             use std::os::unix::io::{FromRawFd, IntoRawFd};
+            #[cfg(target_os = "hermit")]
+            use std::os::hermit::io::{FromRawFd, IntoRawFd};
 
             let raw_fd = self.inner.into_raw_fd();
             unsafe { mio::net::TcpListener::from_raw_fd(raw_fd) }
@@ -945,9 +957,12 @@ impl TcpSocket {
     /// }
     /// ```
     pub fn from_std_stream(std_stream: std::net::TcpStream) -> TcpSocket {
-        #[cfg(unix)]
+        #[cfg(any(unix, target_os = "hermit"))]
         {
+            #[cfg(unix)]
             use std::os::unix::io::{FromRawFd, IntoRawFd};
+            #[cfg(target_os = "hermit")]
+            use std::os::hermit::io::{FromRawFd, IntoRawFd};
 
             let raw_fd = std_stream.into_raw_fd();
             unsafe { TcpSocket::from_raw_fd(raw_fd) }
