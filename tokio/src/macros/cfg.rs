946deb3530774e07f5f3e21eb3a51d515058dfd4
Louis Vialar <louis.vialar@gmail.com>
2025-09-06 14:07:35 +0200

    hermit: disable signals and process when using hermit

--- tokio/src/macros/cfg.rs
+++ tokio/src/macros/cfg.rs
@@ -316,6 +316,7 @@ macro_rules! cfg_process {
             #[cfg_attr(docsrs, doc(cfg(feature = "process")))]
             #[cfg(not(loom))]
             #[cfg(not(target_os = "wasi"))]
+            #[cfg(not(target_os = "hermit"))]
             $item
         )*
     }
@@ -345,6 +346,7 @@ macro_rules! cfg_signal {
             #[cfg_attr(docsrs, doc(cfg(feature = "signal")))]
             #[cfg(not(loom))]
             #[cfg(not(target_os = "wasi"))]
+            #[cfg(not(target_os = "hermit"))]
             $item
         )*
     }
